50fe130af3a7187c88a424dd9d0ce260f812fba6
BenjaminCoajou <coajou.benjamin@gmail.com>
2020-01-22 11:43:13 +0100

    liens dynamique de la home

--- app/views/home.tpl.php
+++ app/views/home.tpl.php
@@ -7,7 +7,7 @@
               alt="Card image" class="card-img">
             <div class="card-img-overlay d-flex align-items-center">
               <div class="w-100 py-3">
-                <h2 class="display-3 font-weight-bold mb-4"><?= $viewVars['homeCategory'][$i]->getName()?></h2><a href="category.html" class="btn btn-light"><?= $viewVars['homeCategory'][$i]->getSubtitle()?></a>
+                <h2 class="display-3 font-weight-bold mb-4"><?= $viewVars['homeCategory'][$i]->getName()?></h2><a href="<?= $router->generate('route_category_by_id', ['categoryId'=> $viewVars['homeCategory'][$i]->getId()]); ?>" class="btn btn-light"><?= $viewVars['homeCategory'][$i]->getSubtitle()?></a>
               </div>
             </div>
           </div>
@@ -21,7 +21,7 @@
               alt="Card image" class="card-img">
             <div class="card-img-overlay d-flex align-items-center">
               <div class="w-100">
-                <h2 class="display-4 mb-4"><?= $viewVars['homeCategory'][$i]->getName()?></h2><a href="category.html" class="btn btn-link text-white"><?= $viewVars['homeCategory'][$i]->getSubtitle()?>
+                <h2 class="display-4 mb-4"><?= $viewVars['homeCategory'][$i]->getName()?></h2><a href="<?= $router->generate('route_category_by_id', ['categoryId'=> $viewVars['homeCategory'][$i]->getId()]); ?>" class="btn btn-link text-white"><?= $viewVars['homeCategory'][$i]->getSubtitle()?>
                   <i class="fa-arrow-right fa ml-2"></i></a>
               </div>
             </div>
@@ -29,35 +29,4 @@
         </div>
         <?php endfor; ?>
 
-<!--      <div class="col-lg-4">
-            <div class="card border-0 text-center text-dark">
-              <img src="<?= $_SERVER['BASE_URI']?>/assets/images/categ1.jpeg"
-                alt="Card image" class="card-img">
-              <div class="card-img-overlay d-flex align-items-center">
-                <div class="w-100">
-                  <h2 class="display-4 mb-4">Détente</h2>
-                  <a href="category.html" class="btn btn-link text-dark">Se faire plaisir
-                    <i class="fa-arrow-right fa ml-2"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        <div class="col-lg-4">
-          <div class="card border-0 text-center text-white">
-            <img src="<?= $_SERVER['BASE_URI']?>/assets/images/categ3.jpeg"
-              alt="Card image" class="card-img">
-            <div class="card-img-overlay d-flex align-items-center">
-              <div class="w-100">
-                <h2 class="display-4 mb-4">Cérémonie</h2>
-                <a href="category.html" class="btn btn-link text-white">Bien choisir 
-                  <i class="fa-arrow-right fa ml-2"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-      </div>
-    </div> -->
   </section>
